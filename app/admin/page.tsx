bc09618365dd2290e7abb01ed075ce214eb4751e
Rahul gogra <rgogra914@gmail.com>
2025-03-05 19:17:18 +0530

    updated admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             You dont have access to this page yet!!<br></br>
             but you can try others pages known as &apos;what we keep hidden from
-            others&apos;
+            others in page of /admin&apos;
         </div>
     );
 }
