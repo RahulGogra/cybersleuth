ba2661133a0996462a53311004169c10e93eef9e
Rahul gogra <rgogra914@gmail.com>
2025-03-05 19:14:13 +0530

    added admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            You dont have access to this page yet!!<br></br>
+            but you can try others pages known as &apos;what we keep hidden from
+            others&apos;
+        </div>
+    );
+}
