a527b525f63de4a7cad3c5a62a88a8d0165b348c
Rahul gogra <rgogra914@gmail.com>
2025-03-06 21:55:24 +0530

    commit

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             You dont have access to this page yet!!<br></br>
             but you can try others pages known as &apos;what we keep hidden from
-            others in page of /admin&apos;
+            others &apos; in place of /admin
         </div>
     );
 }
